2bc995a57aaf1570104bd1bb6fab51eaf34fb7d0
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-04-18 11:38:33 -0300

    Implement CRUD operations for user management in the API

--- api/cotroller/user.js
+++ api/cotroller/user.js
@@ -12,3 +12,48 @@ export const getUsers = (_,res) =>{
         return res.status(200).json(data);
     });
 };
+export const addUser = (req,res) =>{
+    const q = "INSERT INTO users(`nome`,`email`,`fone`,`data_nascimento` ) VALUES (?)";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q,[values], (err,data) => {
+
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário criado com sucesso!");
+    });
+}
+export const updateUser = (req,res) =>{
+    const q = "UPDATE users SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE id = ?";
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q,[...values,req.params.id], (err,data) => {
+
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário atualizado com sucesso!");
+    });
+}
+
+export const deleteUser = (req,res) =>{
+    const q = "DELETE FROM users WHERE id = ?";
+
+    db.query(q,[req.params.id], (err,data) => {
+
+        if(err) return res.json(err);
+
+        return res.status(200).json("Usuário deletado com sucesso!");
+    });
+}
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getUsers } from "../cotroller/user.js";
+import { getUsers,addUser,updateUser,deleteUser } from "../cotroller/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser); 
 
 export default router;
